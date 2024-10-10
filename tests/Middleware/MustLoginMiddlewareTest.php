2875bd5ed0fac9bc59b9e7fefb2d6d19ea7df772
Mochamad Wahyu <wahyu212122@gmail.com>
2024-10-10 08:56:44 +0700

    Middleware

---
+++ tests/Middleware/MustLoginMiddlewareTest.php
@@ -0,0 +1,18 @@
+<?php   
+namespace MochamadWahyu\Phpmvc\MiddleWare;
+
+
+use PHPUnit\Framework\TestCase;
+
+class MustLoginMiddlewareTest extends TestCase
+{
+    private MustLoginMiddleware $mustLoginMiddleware;
+
+    protected function setUp(): void
+    {
+        $this->mustLoginMiddleware = new MustLoginMiddleware();
+    }
+    
+    public function testBefore(){}
+    
+}
