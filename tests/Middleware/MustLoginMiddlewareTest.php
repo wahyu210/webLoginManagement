2339797aa72f6fcd7957778865a1636b1e072cee
wahyu <wahyu212122@gmail.com>
2024-10-10 21:37:44 +0700

    middleware

--- tests/Middleware/MustLoginMiddlewareTest.php
+++ tests/Middleware/MustLoginMiddlewareTest.php
@@ -2,21 +2,31 @@
 namespace MochamadWahyu\Phpmvc\MiddleWare;
 
 
+use MochamadWahyu\Phpmvc\Repository\SessionRepository;
+use MochamadWahyu\Phpmvc\Repository\UserRepository;
+use MochamadWahyu\Phpmvc\Config\Database;
 use PHPUnit\Framework\TestCase;
 
 class MustLoginMiddlewareTest extends TestCase
 {
     private MustLoginMiddleware $mustLoginMiddleware;
+    private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
         $this->mustLoginMiddleware = new MustLoginMiddleware();
         putenv("mode=test");
+
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+
     }
     
-    public function testBefore(){
+    public function testBeforeGuess(){
         $this->mustLoginMiddleware->before();
-        $this->expectOutputString('');
+        $this->expectOutputString('[Location : /users/login]');
     }
-    
+     public function testBeforeLoginUser(){
+     }
 }
